c080b5719c68af309de82693a808665be375b7a7
RapNautico <foox299@gmail.com>
2021-12-21 16:36:35 -0500

    Cotizador de prestamos hecho en react native finalizado

---
+++ src/components/Result.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Result(props) {
+    const{errorMessage, capital, interest, months, total} = props;
+    return (
+        <View style={styles.content}> 
+            {total && (
+                <View style={styles.boxResult}>
+                   <Text style={styles.title}>Resumen</Text> 
+                   <DataResult title="Cantidad solicitada: " value={`${capital} $`}/>
+                   <DataResult title="Interes %: " value={`${interest} %`}/>
+                   <DataResult title="Plazos: " value={`${months} meses`}/>
+                   <DataResult title="Pago mensual: " value={`${total.monthlyFee} $`}/>
+                   <DataResult title="Total a pagar: " value={`${total.totalPay} $`}/>
+                </View>
+            )}
+            <View>
+                <Text style={styles.errorMessage}>{errorMessage}</Text>
+            </View>
+        </View>
+    )
+}
+function DataResult(props) {
+    const {title, value } = props;
+
+    return (
+        <View style={styles.value}>
+            <Text>{title}</Text>
+            <Text>{value}</Text>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    content: {
+        marginTop: 5,
+        marginHorizontal: 40,
+    },
+    errorMessage: {
+        textAlign: 'center',
+        color: '#f00',
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    boxResult: {
+        padding: 30,
+    },
+    title: {
+        fontSize: 30,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        marginBottom: 30,
+    },
+    value: {
+        fontSize: 28,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+    }
+})
---
+++ src/utils/colors.js
@@ -0,0 +1,4 @@
+export default {
+    PRIMARY_COLOR: '#689f38',
+    PRIMARY_COLOR_DARK: '#387002'
+}
